a717c8ecad11ea2716765da0a0f04bfe1dddfbd6
Vipul Khandelwal <khandelwal.vipul1605@gmail.com>
2020-07-30 16:01:48 +0530

    Updates

---
+++ precache-manifest.ed0aad4a144548c6219098a5a790aafd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "92ca7b7f4186caadf26f",
+    "url": "/Colors_App/static/css/main.eb95f4bf.chunk.css"
+  },
+  {
+    "revision": "92ca7b7f4186caadf26f",
+    "url": "/Colors_App/static/js/main.db40f7b1.chunk.js"
+  },
+  {
+    "revision": "80fc1162f4fbe156118e",
+    "url": "/Colors_App/static/js/runtime~main.c2df2120.js"
+  },
+  {
+    "revision": "7f7f0ff7c95351700a33",
+    "url": "/Colors_App/static/css/2.a863de65.chunk.css"
+  },
+  {
+    "revision": "7f7f0ff7c95351700a33",
+    "url": "/Colors_App/static/js/2.a0bf7524.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/Colors_App/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "e25659fdb80d6dd7635602837a3610ae",
+    "url": "/Colors_App/index.html"
+  }
+];
